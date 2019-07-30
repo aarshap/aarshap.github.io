2fee2e81bed0a91739280a3bd3b86b3a240e9e6f
Aaron Shapiro <aaron@aaron.mn>
2019-07-29 21:13:27 -0400

    Image lazy loading on project pages

--- javascripts/core.js
+++ javascripts/core.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+document.addEventListener("DOMContentLoaded", function() {
+  yall({
+    observeChanges: true,
+    lazyClass: 'media-lazy',
+    events: {
+      load: event => {
+        if (event.target.nodeName == "IMG" && !event.target.classList.contains("media-lazy")) {
+          event.target.classList.add("media-loaded");
+        }
+      }
+    }
+  });
+});
