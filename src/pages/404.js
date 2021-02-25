2991c5817c3cf57d341b74f7a47be6f4a4f53348
Aaron Shapiro <aaron@aaron.mn>
2021-02-25 16:39:34 -0500

    Fixed the big, huge update

--- src/pages/404.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Error404 from "../components/Error404"
-
-export default () => {
-    return (
-        <Error404 />
-    )
-}
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Index from "../components/Index"
-
-export default () => {
-    return (
-        <Index />
-    )
-}
